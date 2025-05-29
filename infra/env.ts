f06e0ee420fbe5ade63dc89f1671c9398e87f9bf
Bartosz Gordon <gordonbartosz@gmail.com>
2025-05-29 20:48:40 +0200

    step: setup infra directory

---
+++ infra/env.ts
@@ -0,0 +1,17 @@
+import * as bg from "@bgord/node";
+import z from "zod";
+
+const EnvironmentSchema = z
+  .object({
+    PORT: bg.Schema.Port,
+    LOGS_LEVEL: bg.Schema.LogLevel,
+    TZ: bg.Schema.TimezoneUTC,
+    BASIC_AUTH_USERNAME: bg.Schema.BasicAuthUsername,
+    BASIC_AUTH_PASSWORD: bg.Schema.BasicAuthPassword,
+  })
+  .strip();
+
+export const Env = new bg.EnvironmentValidator<z.infer<typeof EnvironmentSchema>>({
+  type: process.env.NODE_ENV,
+  schema: EnvironmentSchema,
+}).load();
---
+++ infra/index.ts
@@ -0,0 +1,63 @@
+import * as bgb from "@bgord/bun";
+import * as bgn from "@bgord/node";
+import { basicAuth } from "hono/basic-auth";
+import { HTTPException } from "hono/http-exception";
+import type { TimingVariables } from "hono/timing";
+import { Env } from "./env";
+
+export * from "./env";
+export * from "./logger";
+
+export const requestTimeoutError = new HTTPException(408, {
+  message: "request_timeout_error",
+});
+
+export type Variables = TimingVariables &
+  bgb.TimeZoneOffsetVariables &
+  bgb.ContextVariables &
+  bgb.EtagVariables;
+
+export const BODY_LIMIT_MAX_SIZE = new bgn.Size({
+  value: 128,
+  unit: bgn.SizeUnit.kB,
+}).toBytes();
+
+export const BasicAuthShield = basicAuth({
+  username: Env.BASIC_AUTH_USERNAME,
+  password: Env.BASIC_AUTH_PASSWORD,
+});
+
+export const ApiKeyShield = new bgb.ApiKeyShield({ API_KEY: Env.API_KEY });
+
+export const prerequisites = [
+  new bgn.PrerequisitePort({ label: "port", port: Env.PORT }),
+  new bgn.PrerequisiteTimezoneUTC({ label: "timezone", timezone: Env.TZ }),
+  new bgn.PrerequisiteRAM({
+    label: "RAM",
+    enabled: Env.type !== bgn.Schema.NodeEnvironmentEnum.local,
+    minimum: new bgn.Size({ unit: bgn.SizeUnit.MB, value: 128 }),
+  }),
+  new bgn.PrerequisiteSpace({
+    label: "disk-space",
+    minimum: new bgn.Size({ unit: bgn.SizeUnit.MB, value: 512 }),
+  }),
+  new bgn.PrerequisiteNode({
+    label: "node",
+    version: bgn.PackageVersion.fromStringWithV("v24.1.0"),
+  }),
+  new bgn.PrerequisiteBun({
+    label: "bun",
+    version: bgn.PackageVersion.fromString("1.2.15"),
+    current: Bun.version,
+  }),
+  new bgn.PrerequisiteMemory({
+    label: "memory-consumption",
+    maximum: new bgn.Size({ value: 300, unit: bgn.SizeUnit.MB }),
+  }),
+];
+
+export const healthcheck = [
+  new bgn.PrerequisiteSelf({ label: "self" }),
+  new bgn.PrerequisiteOutsideConnectivity({ label: "outside-connectivity" }),
+  ...prerequisites.filter((prerequisite) => prerequisite.config.label !== "port"),
+];
