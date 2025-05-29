73abbed3c2fb30ddc03fc45fdbd2df0ed05a8b7e
Bartosz Gordon <gordonbartosz@gmail.com>
2025-05-29 20:44:17 +0200

    step: setup index.ts

---
+++ index.ts
@@ -0,0 +1,24 @@
+import * as bgb from "@bgord/bun";
+import * as bgn from "@bgord/node";
+import * as infra from "./infra";
+import { server, startup } from "./server";
+
+(async function main() {
+  await bgn.Prerequisites.check(infra.prerequisites);
+
+  const app = Bun.serve({
+    fetch: server.fetch,
+    maxRequestBodySize: infra.BODY_LIMIT_MAX_SIZE,
+  });
+
+  infra.logger.info({
+    message: "Server has started",
+    operation: "server_startup",
+    metadata: {
+      port: infra.Env.PORT,
+      startupTimeMs: startup.stop().durationMs,
+    },
+  });
+
+  bgb.GracefulShutdown.applyTo(app);
+})();
